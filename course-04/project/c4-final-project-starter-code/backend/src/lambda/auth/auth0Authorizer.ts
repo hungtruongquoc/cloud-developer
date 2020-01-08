7210d70a9ce06793f7c96fd0dc119dfd81c8476d
Hung Truong <htruong@Jp3-Macpro.local>
2020-01-08 14:23:19 -0600

    20200108: Removed duplicate API name
    20200108: Added direnv config to autoload environment variable
    
    20200108: Added new packages for AWS type hinting
    
    20200108: Added options for CORS
    
    20200108: Added URL for JWK verification

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-gfdym553.auth0.com/api/v2/'
 
 export const handler = async (
   event: CustomAuthorizerEvent
