e47b91f391468ba8526bc0fd5e0cc1942a51f467
Hung Truong <htruong@Jp3-Macpro.attlocal.net>
2020-01-18 15:35:21 -0600

    20200118: Added config for front-end app
    20200118: Added environment variables and secret for the authorization process

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '1vd7qlbil5'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-gfdym553.auth0.com',            // Auth0 domain
+  clientId: 'lWwZ0BSlXRDQ23uaf4kFCfi2uK7O2wCC',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
