42f096cb68eeb6872dc4dbce91960bb135d5c376
Hung Truong <hungtruongquoc@gmail.com>
2020-01-27 16:30:55 -0600

    20200127: Added code for generating upload URL

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,53 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import {APIGatewayProxyResult, APIGatewayProxyHandler, APIGatewayProxyEvent} from 'aws-lambda'
+import {createLogger} from "../../utils/logger";
+import {getSecretValue} from "../utils";
+import * as middy from 'middy'
+import {cors} from "middy/middlewares";
 
-import { APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const logger = createLogger('get_upload_url')
 
-export const handler: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
-  // const todoId = event.pathParameters.todoId
+let uploadSecret: string = null
+let uploadAccess: string = null
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const todoId = event.pathParameters.todoId
+	const currentUploadSecret = await getUploadSecret()
+	const currentUploadAccess = await getUploadAccess()
+	const s3 = new AWS.S3({
+		secretAccessKey: currentUploadSecret,
+		accessKeyId: currentUploadAccess
+	})
+  const params = {Bucket: 'serverless-todo-app-asset', ContentType: 'image/jpeg', Key: `${todoId}.jpeg`};
+	const uploadUrl = s3.getSignedUrl('putObject', params)
+	// TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+	logger.info('Result: ', {todoId, uploadUrl})
+	return {
+		statusCode: 200,
+		body: JSON.stringify({uploadUrl})
+	}
+})
+
+// @ts-ignore
+handler.use(cors({credentials: true}))
+
+async function getUploadSecret() {
+	if (uploadSecret) return uploadSecret
+
+	logger.info('Environment variables: ', process.env)
+	uploadSecret =  await getSecretValue(process.env.UPLOAD_SECRET_ID, process.env.UPLOAD_SECRET_FIELD)
+	logger.info('Upload secret', {uploadSecret})
+
+	return uploadSecret
+}
+
+async function getUploadAccess() {
+	if (uploadAccess) return uploadAccess
+
+	logger.info('Environment variables: ', process.env)
+	uploadAccess = await getSecretValue(process.env.UPLOAD_ACCESS_ID, process.env.UPLOAD_ACCESS_FIELD)
+	logger.info('Upload access', {uploadAccess})
+
+	return uploadAccess
 }
