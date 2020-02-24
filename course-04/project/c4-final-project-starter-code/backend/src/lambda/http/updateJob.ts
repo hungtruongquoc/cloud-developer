39b32d01b3cfef79b2d76f3910b2ff7202e9c0da
Hung Truong <hungtruongquoc@gmail.com>
2020-02-24 11:28:32 -0600

    20200224: Added the updating functionality

---
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateJob.ts
@@ -0,0 +1,29 @@
+import 'source-map-support/register'
+
+import {APIGatewayProxyHandler, APIGatewayProxyResult, APIGatewayProxyEvent} from 'aws-lambda'
+
+import * as middy from 'middy'
+import {cors} from 'middy/middlewares'
+import {createLogger} from "../../utils/logger";
+import {updateJob} from "../../businessLogic/jobs";
+import {getUserId} from "../../utils/jwtToken";
+
+const logger = createLogger('job-update')
+
+export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	const {jobId} = event.pathParameters
+	const updatedJob: any = JSON.parse(event.body)
+	const authorization = event.headers.Authorization
+	const split = authorization.split(' ')
+	const jwtToken = split[1]
+	const user = getUserId(jwtToken)
+	logger.info('Update item: ', {jobId, updatedJob});
+	const result = await updateJob(user, jobId, updatedJob)
+	logger.info('Update result: ', {result})
+	return {
+		statusCode: 200,
+		body: JSON.stringify({item: result})
+	}
+})
+// @ts-ignore
+handler.use(cors({credentials: true}));
--- course-04/project/c4-final-project-starter-code/client/src/components/JobDetail.tsx
+++ course-04/project/c4-final-project-starter-code/client/src/components/JobDetail.tsx
@@ -1,23 +1,35 @@
-import * as React from "react";
-import Auth from "../auth/Auth";
-import {History} from "history";
-import {Button, Form, Icon, Input, InputOnChangeData, Modal, TextArea, TextAreaProps} from "semantic-ui-react";
+import * as React from 'react'
+import Auth from '../auth/Auth'
+import {History} from 'history'
+import {
+  Button,
+  Form,
+  Icon,
+  Input,
+  InputOnChangeData,
+  Modal,
+  TextArea,
+  TextAreaProps
+} from 'semantic-ui-react'
+import {ChangeEvent, FormEvent} from "react";
 
 interface ModalProps {
   auth: Auth
   history: History
-  addNewJob: any
+  addNewJob?: any
   initialName: string
   initialDesc: string
-  initialId: string
+  initialId: string,
+  updateJob?: any
 }
 
 interface DetailState {
-  showCreateDialog: boolean
-  newJobName: string
-  newJobDesc: string
-  hasError: boolean
-  jobId: string
+  showCreateDialog?: boolean
+  newJobName?: string
+  newJobDesc?: string
+  hasError?: boolean
+  jobId?: string,
+  isLoading?: boolean
 }
 
 export class JobDetail extends React.PureComponent<ModalProps, DetailState> {
@@ -26,33 +38,39 @@ export class JobDetail extends React.PureComponent<ModalProps, DetailState> {
     newJobDesc: '',
     newJobName: '',
     hasError: false,
-    jobId: ''
+    jobId: '',
+    isLoading: false
   }
 
   constructor(props: ModalProps) {
-    super(props);
-    const {initialDesc, initialId, initialName} = props;
+    super(props)
+    const {initialDesc, initialId, initialName} = props
     if (initialId) {
-      this.state.jobId = initialId;
+      this.state.jobId = initialId
     }
     if (initialDesc) {
-      this.state.newJobDesc = initialDesc;
+      this.state.newJobDesc = initialDesc
     }
     if (initialName) {
-      this.state.newJobName = initialName;
+      this.state.newJobName = initialName
     }
+  }
+
+  get isEditMode() {
+    const {jobId} = this.state;
+    return jobId && jobId != ''
   }
 
   showCreateDialog = () => {
-    this.setState({showCreateDialog: true});
+    this.setState({showCreateDialog: true})
   }
 
   closeCreateDialog = () => {
-    this.setState({showCreateDialog: false});
+    this.setState({showCreateDialog: false})
   }
 
   clearForm = () => {
-    this.setState({newJobName: '', newJobDesc: ''});
+    this.setState({newJobName: '', newJobDesc: ''})
   }
 
   clearThenCloseDialog = () => {
@@ -61,58 +79,85 @@ export class JobDetail extends React.PureComponent<ModalProps, DetailState> {
   }
 
   addNewJobThenClose = () => {
-    this.setState((prevState, props) => {
-      const {addNewJob} = props;
-      const {newJobDesc, newJobName} = prevState;
-      if (addNewJob) {
-        addNewJob(newJobName, newJobDesc);
-      }
-      return {showCreateDialog: false, newJobDesc: '', newJobName: ''};
+    this.setState({isLoading: true}, () => {
+      setTimeout(() => {
+        this.setState((prevState, props) => {
+          const {addNewJob, updateJob} = props
+          const {newJobDesc, newJobName, jobId} = prevState
+          if (this.isEditMode) {
+            if (updateJob) {
+              updateJob(jobId, newJobDesc, newJobName)
+            }
+            return {showCreateDialog: false}
+          }
+          if (addNewJob) {
+            addNewJob(newJobName, newJobDesc)
+          }
+          return {showCreateDialog: false, newJobDesc: '', newJobName: ''}
+        })
+      }, 1000);
     })
   }
 
   // @ts-ignore
-  handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, {name, value}: InputOnChangeData | TextAreaProps) => this.setState({[name]: value})
+  handleChange = (
+    e: ChangeEvent<HTMLInputElement> | FormEvent<HTMLTextAreaElement>,
+    {name, value}: InputOnChangeData | TextAreaProps
+  ) => this.setState({[name]: value})
 
   render = () => {
-    const {showCreateDialog, newJobName, newJobDesc, jobId} = this.state;
-    const hasError = !newJobName || newJobName == '';
-    const isEditMode = jobId && jobId != '';
-    const iconName = isEditMode ? 'pencil' : 'plus circle';
+    const {showCreateDialog, newJobName, newJobDesc, isLoading} = this.state
+    const hasError = !newJobName || newJobName == ''
+    const {isEditMode} = this;
+    const iconName = isEditMode ? 'pencil' : 'plus circle'
     const triggerButton = isEditMode ? (
-      <Button icon color='blue' onClick={this.showCreateDialog}>
+      <Button icon color="blue" onClick={this.showCreateDialog}>
         <Icon name={iconName}/>
       </Button>
-    ) : (<Button icon labelPosition='left' color='orange' onClick={this.showCreateDialog}>
-      <Icon name={iconName}/>
-      Post a New Job
-    </Button>);
-    const header = isEditMode ? `Update Job Post ${newJobName}` : `Post a New Job`;
+    ) : (
+      <Button
+        icon
+        labelPosition="left"
+        color="orange"
+        onClick={this.showCreateDialog}
+      >
+        <Icon name={iconName}/>
+        Post a New Job
+      </Button>
+    )
+    const header = isEditMode ? `Update Job Post ${newJobName}` : `Post a New Job`
 
-    return (<Modal trigger={triggerButton} closeOnEscape={false} closeOnDimmerClick={false} open={showCreateDialog}>
-      <Modal.Header>{header}</Modal.Header>
-      <Modal.Content>
-        <Form error={hasError}>
-          <Form.Field required>
-            <label>Name</label>
-            <Input placeholder='Provide a name' name='newJobName' fluid value={newJobName}
-                   onChange={this.handleChange}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Job Description</label>
-            <TextArea placeholder='Tell us your job description' name='newJobDesc' rows={5} value={newJobDesc}
-                      onChange={this.handleChange}/>
-          </Form.Field>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='red' onClick={this.clearThenCloseDialog}>
-          <Icon name='remove'/> Cancel
-        </Button>
-        <Button color='green' disabled={hasError} onClick={this.addNewJobThenClose}>
-          <Icon name='checkmark'/> {isEditMode ? 'Update' : 'Post'}
-        </Button>
-      </Modal.Actions>
-    </Modal>);
+    return (
+      <Modal
+        trigger={triggerButton}
+        closeOnEscape={false}
+        closeOnDimmerClick={false}
+        open={showCreateDialog}
+      >
+        <Modal.Header>{header}</Modal.Header>
+        <Modal.Content>
+          <Form error={hasError}>
+            <Form.Field required>
+              <label>Name</label>
+              <Input placeholder="Provide a name" name="newJobName" fluid value={newJobName}
+                     onChange={this.handleChange}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Job Description</label>
+              <TextArea placeholder="Tell us your job description" name="newJobDesc" rows={5} value={newJobDesc}
+                        onChange={this.handleChange}/>
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="red" onClick={this.clearThenCloseDialog} disabled={isLoading}>
+            <Icon name="remove"/> Cancel
+          </Button>
+          <Button color="green" disabled={hasError} onClick={this.addNewJobThenClose} loading={isLoading}>
+            <Icon name="checkmark"/> {isEditMode ? 'Update' : 'Post'}
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
   }
 }
